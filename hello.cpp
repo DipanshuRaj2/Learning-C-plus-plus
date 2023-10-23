869cdba78b416b6ec5f8de4d608dfa5e2e82176a
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2023-10-24 02:05:48 +0530

    meassge

---
+++ hello.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    //print the reverse the of multiplier
+    // int n = 3 , count =10;
+    // for(int i = 30; i>=3; i=i-3){
+    //     cout << n <<"*"<< count<<"="<<i;
+    //     count--;
+    //     cout << endl;
+    //     }
+    // int n = 30, i= 3, count= 10;
+    // while(n>=3){
+    //     cout <<i<<"*"<<count<<"="<< n<<" ";
+    //     n=n-3;
+    //     count--;
+    //     cout<<endl
+    // cout <<"for even number";
+    // for(int i = 1; i<=100; i=i+2)
+    //     cout << i<<" ";
+    //     cout<<endl;
+
+    // //for odd number
+    // for(int i = 2; i<=100; i=i+2){
+    //     cout << i<<" "; 
+    // }
+    // cout << endl;
+
+    // for(int i = 100; i>=2; i=i-2)
+    // cout << i<<endl;
+
+    // int a  = 10; // a = 10;
+    // int b = 30; // b = 30;
+    // int temp;
+
+    // temp = a;
+    // a=b;
+    // b=temp;
+    // cout<< a << endl;
+    // cout << b<< endl;
+
+    int a = 3;
+    int b =5;
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    cout << a<<"\n"<<b;
+
+
+
+     
+    }
+
