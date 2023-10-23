246fbb1f352588e2dfdbe330dca745cd91917f0a
Dipanshu Raj <118935913+DipanshuRaj2@users.noreply.github.com>
2023-10-24 02:00:59 +0530

    message

--- hello.cpp
+++ hello.cpp
@@ -1,37 +1,54 @@
-#include<iostream>
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std ;
 int main()
 {
-	
-	//cout<<"hello world" <<endl;
-	//cout<<"next line";
-	
-	/*int a, b, c;
-	short sa=9;
-	short _Sa=9;
-	cout<<sa;*/
-	
-//	int marksinmaths = 83 ;
-//	cout<<"the marks of students in maths is"<< marksinmaths <<endl;
-//	float score=23.09;
-//   	cout<<score;
-	
-	/*int b=4;
-	 cout<< b<<endl;
-	 */
-     int a,b;
-	
-	cout<<"enter the first number"<<endl;
-	
-	/*cin>>a;
-	cout<<"enter the second number"<<endl;
-	cin>>b;
-	cout<<"a + b = "<<a+b <<endl;
-	cout<<"a - b ="<<a-b <<endl;
-	cout<<"a * b = "<<a*b <<endl;
-	cout<<"a / b = "<<(float)a/b <<endl;*/
-	
-	
-	
-}
+    //print the reverse the of multiplier
+    // int n = 3 , count =10;
+    // for(int i = 30; i>=3; i=i-3){
+    //     cout << n <<"*"<< count<<"="<<i;
+    //     count--;
+    //     cout << endl;
+    //     }
+    // int n = 30, i= 3, count= 10;
+    // while(n>=3){
+    //     cout <<i<<"*"<<count<<"="<< n<<" ";
+    //     n=n-3;
+    //     count--;
+    //     cout<<endl
+    // cout <<"for even number";
+    // for(int i = 1; i<=100; i=i+2)
+    //     cout << i<<" ";
+    //     cout<<endl;
+
+    // //for odd number
+    // for(int i = 2; i<=100; i=i+2){
+    //     cout << i<<" "; 
+    // }
+    // cout << endl;
+
+    // for(int i = 100; i>=2; i=i-2)
+    // cout << i<<endl;
+
+    // int a  = 10; // a = 10;
+    // int b = 30; // b = 30;
+    // int temp;
+
+    // temp = a;
+    // a=b;
+    // b=temp;
+    // cout<< a << endl;
+    // cout << b<< endl;
+
+    int a = 34;
+    int b =56;
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    cout << a<<"\n"<<b;
+
+
+
+     
+    }
 
