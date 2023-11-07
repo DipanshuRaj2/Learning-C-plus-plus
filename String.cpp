7eb2a1484e7a7e22678736ae93c6ad3222638dbe
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2023-11-08 00:39:45 +0530

    getline

--- String.cpp
+++ String.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     string s;
-    getline(cin, s);
+    getline(cin, s); // getline important concept of string to restore the string the value
     cout << s<<endl;
     cout << s.size();
 
