59fca5d3bbb1ae0125aa66cddfa95b970999c675
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-31 23:15:06 +0530

    modify the programm

---
+++ REVISION/con-dec-bin.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+// int decimalToBinary(int &n){
+//     int ans = 0;
+//     int i = 0;
+//     while(n != 0){
+//         int digit = n & 1;
+//         ans = (digit * pow(10, i) ) + ans;
+
+//         n = n >> 1;
+//         i++;
+//     }
+//     return ans;
+// }
+int main(){
+    int n;
+    cin >> n;
+    // cout<<decimalToBinary(n);
+    int ans = 0;
+    int i = 0;
+    while(n != 0){
+
+        int bit = n & 1;
+
+        ans = (bit * pow(10, i) ) + ans;
+
+        n = n >> 1;
+
+        i++;
+    }
+    cout<< ans;
+    return 0;
+}
