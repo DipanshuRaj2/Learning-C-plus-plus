cb90d9c550cc7dbecd1365120209adebd5e6ee9c
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-22 10:09:39 +0530

    even or odd

--- REVISION/max_sub_array.cpp
+++ REVISION/max_sub_array.cpp
@@ -1,22 +1,41 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int> maxSubArray(vector<int>v){
+//     int sum = 0;
+//     int subArray= v[0];
+//     vector<int>ans;
+//     for(int i = 0; i<v.size(); i++){
+//         sum = sum+ v[i];
+//         subArray = max(subArray, sum);
+        
+//         if(sum <= 0){
+//             sum = 0;
+//         }
+//     }
+//     return subArray;
+// }
+// int main()
+// {
+//     vector<int>v ={5,20,-30,60,-15,20};
+//     cout<<maxSubArray(v);
+//    return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> maxSubArray(vector<int>v){
-    int sum = 0;
-    int subArray= v[0];
-    vector<int>ans;
-    for(int i = 0; i<v.size(); i++){
-        sum = sum+ v[i];
-        subArray = max(subArray, sum);
-        
-        if(sum <= 0){
-            sum = 0;
-        }
+void checkEvenOrodd(int n){
+    int result = n & 1;
+    if(result == 0){
+        cout<< "Even";
     }
-    return subArray;
+    else{
+     cout<<"odd";
+    }
+    
 }
 int main()
 {
-    vector<int>v ={5,20,-30,60,-15,20};
-    cout<<maxSubArray(v);
+    int n = 5;
+    checkEvenOrodd(n);
    return 0;
 }
